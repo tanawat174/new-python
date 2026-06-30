9037d6950d8154f1ebd8ea68e0259161588ca3cb
tanawat174 <tanaw1865@gmail.com>
2026-06-30 15:38:06 +0700

    EndLecture1

---
+++ lecture-1/hello.py
@@ -0,0 +1,36 @@
+print("hello")
+print("Information and Network Engineering")
+print("Tanawat You will be great")
+
+print("I'm Hungry")
+print("He ask me \"Who got the books\"")
+print("I'm good and \"I will do my best\"")
+
+print("/\\")
+print('\ta\na\ta\ta\n\ta')
+print('''\ta
+a\ta\ta
+\ta''')
+
+print('100.00')
+print('%d' %100)
+print('%d' %100.58)
+print('%f' %-100.89)
+print('%.2f'%100.89)
+
+from math import pi
+print('pi = %.2f' %pi)
+
+print('My age is',25,'I have', 3500.50,'Bth.')
+print('My age is %d I have %.2f Bth.' %(25, 3500.50))
+print('My age is '+ str(25)+' I have '+str(3500.50)+' Bth.')
+
+print('5+4 =' ,(5+4))
+print('5+4 = %.2f' %(5+4))
+print('5+4 = ' +str(5+4))
+
+print("I'm Tanawat", "I'll keep practicing!")
+print("I'm Tanawat " + "I'll keep practicing!")
+
+print(1, 1, 2, 3, 5, 8, 13, 21, 34, 55)
+print('%d %d %d %d %d %d %d %d %d %d' %(1,1,2,3,5,8,13,21,34,55))
